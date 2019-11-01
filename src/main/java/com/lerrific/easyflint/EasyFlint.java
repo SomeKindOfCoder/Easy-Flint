e6fbdfa664e64ce7e9f7af100f403a6ae7a81c0b
lerrific <43840962+lerrific@users.noreply.github.com>
2019-11-01 19:47:36 +1100

    Add files via upload

--- src/main/java/com/lerrific/easyflint/EasyFlint.java
+++ src/main/java/com/lerrific/easyflint/EasyFlint.java
@@ -2,39 +2,45 @@ package com.lerrific.easyflint;
 
 import java.util.Random;
 
-import net.minecraft.block.GravelBlock;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.BlockGravel;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.util.Hand;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod("easyflint")
-public final class EasyFlint {
+// Am I doing this right? -\(o_o)/-
+@Mod(modid = "easyflint", name = "Easy Flint", version = "1.0.0")
+public class EasyFlint
+{	
 	public static final Random rng = new Random();
 
-	public EasyFlint() {
-		MinecraftForge.EVENT_BUS.register(this);
-	}
-
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	MinecraftForge.EVENT_BUS.register(this);
+    }
+    
 	@SubscribeEvent
 	public void rightClickGravel(RightClickBlock event) {
-		if (event.getWorld().getBlockState(event.getPos()).getBlock() instanceof GravelBlock && !event.getPlayer().isCreative() && event.getPlayer().getHeldItem(Hand.MAIN_HAND).isEmpty() && !event.getWorld().isRemote) {
+		if (event.getWorld().getBlockState(event.getPos()).getBlock() instanceof BlockGravel && !event.getEntityPlayer().isCreative() && event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND).isEmpty() && !event.getWorld().isRemote) {
 			World world = event.getWorld();
 			BlockPos pos = event.getPos();
 			
-            world.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_GRAVEL_HIT, SoundCategory.BLOCKS, 0.8F, 1.1F);
+            world.playSound((EntityPlayer)null, pos, SoundEvents.BLOCK_GRAVEL_HIT, SoundCategory.BLOCKS, 0.8F, 1.1F);
 			if (rng.nextInt(100) < 13) {
 				world.destroyBlock(pos, false);
-				world.addEntity(new ItemEntity(world, pos.getX(), pos.getY() + rng.nextDouble(), pos.getZ(), new ItemStack(Items.FLINT, 1)));
+				world.spawnEntity(new EntityItem(world, pos.getX(), pos.getY() + rng.nextDouble(), pos.getZ(), new ItemStack(Items.FLINT, 1)));
 			}
 		}
 	}
